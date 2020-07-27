6eb482d09a0744d4a41090f7bed1670ce1ba8613
Anthony Wilkes <a.sjwilkes@gmail.com>
2020-07-27 16:17:30 +0200

    Fix REPL default prompt from always saying "IntRepl"

--- src/repls/REPL.scala
+++ src/repls/REPL.scala
@@ -2,18 +2,18 @@ package repls
 
 trait REPL {
 
-  val replName : String
+    val replName : String
 
-  def readEval(command : String) : String
+    def readEval(command : String) : String
 
-  def run() : Unit = {
-    while(true) {
-      println(replName + "repls.IntRepl>")
-      val s = scala.io.StdIn.readLine().trim
+    def run() : Unit = {
+        while(true) {
+            println("repls." + replName + ">")
+            val s = scala.io.StdIn.readLine().trim
 
-      if(s == "exit") return
-      println(readEval(s))
+            if(s == "exit") return
+            println(readEval(s))
+        }
     }
-  }
 
 }
