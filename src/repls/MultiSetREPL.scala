6432734bcfc626da32b97174de487bf4fdca508f
Anthony Wilkes <a.sjwilkes@gmail.com>
2020-07-27 21:28:38 +0200

    Add skeleton classes for Int and MultiSet repls

---
+++ src/repls/MultiSetREPL.scala
@@ -0,0 +1,11 @@
+package repls
+
+class MultiSetREPL extends REPLBase {
+    override type Base = MultiSet[Int]
+    override val replName: String = "" // TODO: name me!
+
+    override def readEval(command: String): String = {
+        // TODO: complete me!
+        ""
+    }
+}
---
+++ src/repls/REPLBase.scala
@@ -0,0 +1,7 @@
+package repls
+
+abstract class REPLBase extends REPL {
+    type Base
+
+    // TODO: put code shared by IntREPL and MultiSetREPL here
+}
