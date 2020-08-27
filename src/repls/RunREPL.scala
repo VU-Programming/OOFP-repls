1e0a273358986414bca87c5fda444a6d0515fbaf
Jens Troost <jtroo@protonmail.com>
2020-08-27 11:52:54 +0200

    Added RunREPL object

---
+++ src/repls/RunREPL.scala
@@ -0,0 +1,14 @@
+// DO NOT MODIFY THIS FILE
+package repls
+
+object RunREPL {
+  def main(args: Array[String]): Unit = {
+    val repl = {
+      val classType = args(0)
+      if (classType == "IntREPL") REPLFactory.makeIntREPL()
+      else if (classType == "MultiSetREPL") REPLFactory.makeMultiSetREPL()
+      else throw new Error("Not a valid REPL")
+    }
+    repl.run()
+  }
+}
