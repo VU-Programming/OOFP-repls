9164dda1c6531f1396293891a9f2e621a78c9627
Atze <atzeus@gmail.com>
2020-09-18 16:03:04 +0200

    Split strings

--- src/repls/REPLBase.scala
+++ src/repls/REPLBase.scala
@@ -3,36 +3,5 @@ package repls
 abstract class REPLBase extends REPL {
     type Base
 
-    // splits string into parts a+bcd-x{a, b , c}  -> Seq("a","+","bcd","-","x","{a, b , c}")
-    def splitExpressionString(exp : String) : Seq[String] = {
-        val builder = Seq.newBuilder[String]
-        var curString = ""
-
-        var inLiteral = false
-        def addNonemptyCurAndReset() : Unit =
-            if(curString.nonEmpty) {
-                builder.addOne(curString)
-                curString = ""
-            }
-
-        for(c <- exp) {
-            if(inLiteral) {
-                if(c == '}') {
-                    curString+= c
-                    addNonemptyCurAndReset()
-                    inLiteral = false
-                }
-            }
-            c match {
-                case '{'                    => inLiteral = true
-                case ' ' | '\t'             =>   addNonemptyCurAndReset()
-                case ',' | '+' | '*' | '-'  => { addNonemptyCurAndReset(); builder.addOne(c.toString)}
-                case _ if c.isLetterOrDigit => curString += c
-                case _                      => throw new Exception("Do not know how to parse " + c )
-            }
-        }
-        builder.result()
-    }
-
     // TODO: put code shared by IntREPL and MultiSetREPL here
 }
---
+++ src/repls/SplitExpressionString.scala
@@ -0,0 +1,56 @@
+package repls
+
+object SplitExpressionString {
+
+  /* Split strings in a way that is handy for this exercise.
+
+  examples:
+
+  ac+bc -> (ac,+,bc)
+  ac + bc -> (ac,+,bc)
+  (ac * 8) + 2 -> ((,ac,*,),+,2)
+
+
+  negative number parsing:
+  89-ff -> (89,-,ff)
+  b - 11 -> (b, - , 11)
+  if - directly borders on a character or digit on the right, but not on the left, is included in the string on the right
+  b + -11 -> (b,+,-11) //
+
+  set parsing: everything between { and } is taken as a single literal string
+
+  {a,bc,d} + {df , sfd , a} -> ({a,bc,d}, + ,{df , sfd , a})
+
+
+   */
+  def splitExpressionString(exp : String) : Seq[String] = {
+    val builder = Seq.newBuilder[String]
+    var curString = ""
+
+    var inLiteral = false
+    def addNonemptyCurAndReset() : Unit =
+      if(curString.nonEmpty) {
+        builder.addOne(curString)
+        curString = ""
+      }
+
+    for(c <- exp) {
+      if(inLiteral) {
+        if(c == '}') {
+          curString+= c
+          addNonemptyCurAndReset()
+          inLiteral = false
+        }
+      }
+      c match {
+        case '{'                    => inLiteral = true
+        case ' ' | '\t'             =>   addNonemptyCurAndReset()
+        case ',' | '+' | '*' | '-'  => { addNonemptyCurAndReset(); builder.addOne(c.toString)}
+        case _ if c.isLetterOrDigit => curString += c
+        case _                      => throw new Exception("Do not know how to parse " + c )
+      }
+    }
+    builder.result()
+  }
+
+}
