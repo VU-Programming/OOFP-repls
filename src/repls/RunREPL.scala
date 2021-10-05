2cec54093804692e226b0b7a1a2e622b13a6680e
Atze <atzeus@gmail.com>
2021-10-05 13:35:32 +0200

    New Run buttons

--- src/repls/RunREPL.scala
+++ src/repls/RunREPL.scala
@@ -1,7 +1,20 @@
 // You do not need to modify this file
 package repls
 
+object RunIntREPL {
+  def main(args: Array[String]): Unit = {
+    REPLFactory.makeIntREPL().run()
+  }
+}
+
+object RunMultiSetREPL {
+  def main(args: Array[String]): Unit = {
+    REPLFactory.makeMultiSetREPL().run()
+  }
+}
+
 object RunREPL {
+
   def main(args: Array[String]): Unit = {
     val repl = {
       val classType = args(0)
