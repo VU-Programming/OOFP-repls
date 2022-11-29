64b4c2a317eb703a1363d590be52dadb246791cf
Atze <atzeus@gmail.com>
2022-11-29 13:45:20 +0100

    Removes confusing "name me todo"

--- src/repls/IntREPL.scala
+++ src/repls/IntREPL.scala
@@ -3,7 +3,7 @@ package repls
 class IntREPL extends REPLBase {
     // Have a REPL of type Int
     type Base = Int
-    override val replName: String = "ints" // TODO: name me!
+    override val replName: String = "int-repl"
 
     override def readEval(command: String): String = {
         val elements = command.split("\\s") // split string based on whitespace
--- src/repls/MultiSetREPL.scala
+++ src/repls/MultiSetREPL.scala
@@ -3,7 +3,7 @@ package repls
 class MultiSetREPL extends REPLBase {
     // Have a REPL of a MutliSet of strings
     override type Base = MultiSet[String]
-    override val replName: String = "multiset" // TODO: name me!
+    override val replName: String = "multiset-repl"
 
     override def readEval(command: String): String = {
         // TODO: complete me!
